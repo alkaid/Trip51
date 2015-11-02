80328c2224f5cee0d81da32a8177df3d4199efe5
alkaid <muliangcong@gmail.com>
2015-11-03 01:19:49 +0800

    bell图标

---
+++ app/src/main/java/com/alkaid/trip51/base/widget/view/NovaImageButton.java
@@ -0,0 +1,29 @@
+
+package com.alkaid.trip51.base.widget.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+
+public class NovaImageButton extends ImageButton
+{
+
+    private android.view.View.OnClickListener mListener;
+
+    public NovaImageButton(Context context)
+    {
+        super(context);
+    }
+
+    public NovaImageButton(Context context, AttributeSet attributeset)
+    {
+        super(context, attributeset);
+    }
+
+    public NovaImageButton(Context context, AttributeSet attributeset, int i)
+    {
+        super(context, attributeset, i);
+    }
+
+}
---
+++ app/src/main/java/com/alkaid/trip51/base/widget/view/NovaTextView.java
@@ -0,0 +1,29 @@
+
+package com.alkaid.trip51.base.widget.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+// Referenced classes of package com.dianping.widget.view:
+//            GAUserInfo, GAHelper
+
+public class NovaTextView extends TextView
+{
+
+    public NovaTextView(Context context)
+    {
+        super(context);
+    }
+
+    public NovaTextView(Context context, AttributeSet attributeset)
+    {
+        super(context, attributeset);
+    }
+
+    public NovaTextView(Context context, AttributeSet attributeset, int i)
+    {
+        super(context, attributeset, i);
+    }
+
+}
