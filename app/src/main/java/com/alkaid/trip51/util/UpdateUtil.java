76d767b45a2e5e3d2a99e38eef07c3506070954e
alkaid <muliangcong@gmail.com>
2015-12-10 21:30:52 +0800

    1.集成友盟自动更新

--- app/src/main/java/com/alkaid/trip51/util/UpdateUtil.java
+++ app/src/main/java/com/alkaid/trip51/util/UpdateUtil.java
@@ -18,42 +18,53 @@ import com.umeng.update.UpdateStatus;
  */
 public class UpdateUtil {
     public static void update(){
-        UmengUpdateAgent.setUpdateOnlyWifi(false);
-        String value = OnlineConfigAgent.getInstance().getConfigParams(App.instance(), "force_upgrade_version_list");
-        if(TextUtils.isEmpty(value)){
-            return;
+        String force_upgrade_mode_value= OnlineConfigAgent.getInstance().getConfigParams(App.instance(), "force_upgrade_mode");
+        boolean forceUpdate=false;
+        if(force_upgrade_mode_value.equals("force_upgrade_version_list")) {
+            String force_upgrade_version_list_value = OnlineConfigAgent.getInstance().getConfigParams(App.instance(), "force_upgrade_version_list");
+            if (TextUtils.isEmpty(force_upgrade_version_list_value)) {
+            }else {
+                String[] force_upgrade_array = force_upgrade_version_list_value.split(";");
+                for (String version : force_upgrade_array) {
+                    version = version.trim();
+                    String versionName = SystemUtil.getSoftVersion(App.instance());
+                    versionName = "v" + versionName;
+                    if (version.equals(versionName)) {
+                        forceUpdate = true;
+                        break;
+                    }
+                }
+            }
         }
-        String[] upgrade_mode_array = value.split(";");
+    }
+    //默认更新策略
+    private static void defaultUpdateStaragy(){
+        //wifi静默下载
+        UmengUpdateAgent.silentUpdate(App.instance());
+    }
+    private static void forceUpdate(){
         UmengUpdateAgent.setUpdateOnlyWifi(false);
         UmengUpdateAgent.update(App.instance());
         UmengUpdateAgent.forceUpdate(App.instance());//这行如果是强制更新就一定加上
-        for(String mode:upgrade_mode_array){
-            mode=mode.trim();
-            String versionName = SystemUtil.getSoftVersion(App.instance());
-            versionName = "v"+versionName;
-            if(mode.equals(versionName)){
-                //进入强制更新
-                UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
-                    @Override
-                    public void onUpdateReturned(int updateStatus, UpdateResponse updateResponse) {
+        //进入强制更新
+        UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+            @Override
+            public void onUpdateReturned(int updateStatus, UpdateResponse updateResponse) {
 
-                    }
-                });
-                UmengUpdateAgent.setDialogListener(new UmengDialogButtonListener() {
-                    @Override
-                    public void onClick(int status) {
-                        switch (status) {
-                            case UpdateStatus.Update:
-                                break;
-                            default:
-                                //退出应用
-                                Toast.makeText(App.instance(),"抱歉，需要更新到版本才能正常使用",Toast.LENGTH_LONG).show();
-                                App.instance().exit();
-                        }
-                    }
-                });
-                break;
             }
-        }
+        });
+        UmengUpdateAgent.setDialogListener(new UmengDialogButtonListener() {
+            @Override
+            public void onClick(int status) {
+                switch (status) {
+                    case UpdateStatus.Update:
+                        break;
+                    default:
+                        //退出应用
+                        Toast.makeText(App.instance(), "抱歉，需要更新到版本才能正常使用", Toast.LENGTH_LONG).show();
+                        App.instance().exit();
+                }
+            }
+        });
     }
 }
